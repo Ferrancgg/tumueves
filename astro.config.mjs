e59f4f80f56bb70dc26020f3823f5b49e5ab0681
Fernando <fernando@MacBook-Pro-de-Fernando.local>
2025-11-15 17:11:03 +0100

    modificaciones para servidor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({});
+
+
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    preview: {
+      // Dominios que pueden acceder a la app en modo preview/producción
+      allowedHosts: ['tumueves.com', 'www.tumueves.com'],
+    },
+  },
+});
