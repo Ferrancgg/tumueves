710d71579a5623771dcd04308ad0249ffd6328eb
Fernando <fernando@MacBook-Pro-de-Fernando.local>
2025-11-15 17:50:13 +0100

    ultimo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,3 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-
-// // https://astro.build/config
-// export default defineConfig({});
-
-
 // @ts-check
 import { defineConfig } from 'astro/config';
 
@@ -12,7 +5,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   vite: {
     preview: {
-      // Dominios que pueden acceder a la app en modo preview/producción
       allowedHosts: ['tumueves.com', 'www.tumueves.com'],
     },
   },
