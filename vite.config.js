a5235e2d7200c02c4f64597260b0cfb9e4033782
Fernando <fernando@MacBook-Pro-de-Fernando.local>
2025-11-17 15:03:50 +0100

    ultimos cambios

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   preview: {
-    // aquí permites tus dominios en producción
     allowedHosts: ['tumueves.com', 'www.tumueves.com'],
   },
   server: {
-    host: true, // expone el server hacia fuera (0.0.0.0)
+    host: true,
+    allowedHosts: ['tumueves.com', 'www.tumueves.com'],
   },
 });
